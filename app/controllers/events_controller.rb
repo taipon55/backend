946bf1155ad4419d621a8cec4750458de40ea07f
taipon55 <Taisei127@outlook.com>
2023-01-26 10:43:58 +0900

    更新削除イベント追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -18,9 +18,26 @@ class EventsController < ApplicationController
           render json: event.errors, status: 422
         end
       end
+
+      def update
+        # 指定したidのイベントデータの値を更新する
+        event = Event.find(params[:id])
+        if event.update(event_params)
+          render json: event
+        else
+          render json: event.errors, status: 422
+        end
+      end
+
+      def destroy
+        #指定したidのイベントデータをっ削除する
+        event = Event.find(params[:id])
+        event.destroy!
+        render json:event
+      end
    
       private
-   
+
       def event_params
         params.require(:event).permit(:id, :name, :start, :end, :timed, :description, :color)
       end
