4924201cdaacbda9d49d5adafe0d836e762ab3ee
taipon55 <Taisei127@outlook.com>
2023-02-06 23:55:55 +0900

    カレンダーAPI作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,5 @@ Event.create!(name: '予定1', start: '2023-02-14 10:00:00', end: '2023-02-15 11
 Event.create!(name: '予定2', start: '2023-02-14 10:00:00', end: '2023-02-15 11:00:00')
 Event.create!(name: '予定3', start: '2023-02-14 10:00:00', end: '2023-02-15 11:00:00')
 Event.create!(name: '予定4', start: '2023-02-14 10:00:00', end: '2023-02-15 11:00:00')
+
+Calendar.create!(name: '予定')
